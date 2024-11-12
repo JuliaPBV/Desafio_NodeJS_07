1f06c57e7dd413d3bc7fd1bf8507c49f94599111
Julia Pereira <vital.pereira.julia@gmail.com>
2024-11-12 11:49:58 -0300

    acrescentei a opção de sair e se não sair continua cadastrando

--- src/index.ts
+++ src/index.ts
@@ -42,7 +42,7 @@ app.listen(PORT, () => {
 
 });
 import readlineSync from 'readline-sync';
-import { Aluno } from "./entities/aluno"; 
+import { Aluno } from "./entities/aluno";
 import { Curso } from "./entities/curso";
 import { AlunoDAO } from "./dao/AlunoDAO";
 import { CursoDAO } from "./dao/cursoDAO";
@@ -51,92 +51,101 @@ async function informações() {
 	try {
 		const cursoDAO = new CursoDAO();
 		const alunoDAO = new AlunoDAO();
-		const opcao = readlineSync.question('O que deseja fazer?\n01- Cadastrar Curso\n02- Cadastrar Aluno\n03- Listar Cursos cadastrados\n04- Listar Alunos Cadastrados\n05- Buscar aluno\n06- buscar curso\nEscolha uma opcao: ');
 
-		switch (opcao) {
-			case "01":
-				const nomeCurso = readlineSync.question('Digite o nome do curso: ');
-				const cursoId = parseInt(readlineSync.question('Digite o ID do curso: '), 10);
-				const curso = new Curso(nomeCurso, cursoId);
+		let opcao: string = ""
+		while (opcao !== "00") {
+			opcao = readlineSync.question('O que deseja fazer?\n01- Cadastrar Curso\n02- Cadastrar Aluno\n03- Listar Cursos cadastrados\n04- Listar Alunos Cadastrados\n05- Buscar aluno\n06- buscar curso\n00- Sair\nEscolha uma opcao: ');
 
-				const existeCurso = await cursoDAO.read(cursoId);
-				if (!existeCurso) {
-					try {
-						await cursoDAO.create(curso);
-						console.log('Curso criado com sucesso!');
-					} catch (error) {
-						console.error('Erro ao criar curso!');
-					}
-				} else {
-					console.log('Curso ja cadastrado sendo utilizado!');
-				}
-				break;
+			switch (opcao) {
+				case "01":
+					const nomeCurso = readlineSync.question('Digite o nome do curso: ');
+					const cursoId = parseInt(readlineSync.question('Digite o ID do curso: '), 10);
+					const curso = new Curso(nomeCurso, cursoId);
 
-			case "02":
-				const nomeAluno = readlineSync.question('Digite o nome do aluno: ');
-				const alunoId = parseInt(readlineSync.question('Digite o ID do aluno: '), 10);
-				const cursoIdAluno = parseInt(readlineSync.question('Digite o ID do curso: '), 10);
-				const aluno = new Aluno( alunoId, nomeAluno, cursoIdAluno);
-
-				const existeAluno = await alunoDAO.read(alunoId);
-				if (!existeAluno) {
-					try {
-						await alunoDAO.create(aluno);
-						console.log('Aluno criado com sucesso!');
-					} catch (error) {
-						console.error('Erro ao criar o aluno:', error);
-					}
-				} else {
-					console.log('Aluno já existente!');
-				}
-				break;
-
-			case "03":
-				const cursos = await cursoDAO.findAll();
-				console.log('Cursos cadastrados: ', cursos);
-				break;
-
-			case "04":
-				const alunos = await alunoDAO.findAll();
-				console.log('Alunos cadastrados: ', alunos);
-				break;
-
-			case "05":
-				const alunoBuscaId = parseInt(readlineSync.question('Digite o ID do aluno: '), 10);
-				const alunoEncontrado = await alunoDAO.read(alunoBuscaId);
-				if (alunoEncontrado) {
-					console.log('Aluno encontrado: ', alunoEncontrado);
-
-					const desejaAlterarNome = readlineSync.question('Deseja alterar o nome do aluno? (sim/nao): ');
-					if (desejaAlterarNome.toLowerCase() === 'sim') {
-						const novoNome = readlineSync.question('Qual é o novo nome do aluno? ');
-						alunoEncontrado.nome = novoNome;
-						await alunoDAO.update(alunoEncontrado);
-						console.log('Nome do aluno atualizado!');
+					const existeCurso = await cursoDAO.read(cursoId);
+					if (!existeCurso) {
+						try {
+							await cursoDAO.create(curso);
+							console.log('Curso criado com sucesso!');
+						} catch (error) {
+							console.error('Erro ao criar curso!');
+						}
 					} else {
-						console.log('Nome do aluno não alterado!');
+						console.log('Curso ja cadastrado sendo utilizado!');
 					}
-				} else {
-					console.log('Aluno não encontrado');
-				}
-				break;
+					break;
 
-			case "06":
-				const cursoBuscarId = parseInt(readlineSync.question('Digite o ID do curso: '), 10);
-				const cursoEncontrado = await cursoDAO.read(cursoBuscarId);
-				if (cursoEncontrado) {
-					console.log('Curso encontrado: ', cursoEncontrado);
-				} else {
-					console.log('Curso não encontrado');
-				}
-				break;
+				case "02":
+					const nomeAluno = readlineSync.question('Digite o nome do aluno: ');
+					const alunoId = parseInt(readlineSync.question('Digite o ID do aluno: '), 10);
+					const cursoIdAluno = parseInt(readlineSync.question('Digite o ID do curso: '), 10);
+					const aluno = new Aluno(alunoId, nomeAluno, cursoIdAluno);
 
-			default:
-				console.log('Opção inválida!!!');
-				return;
+					const existeAluno = await alunoDAO.read(alunoId);
+					if (!existeAluno) {
+						try {
+							await alunoDAO.create(aluno);
+							console.log('Aluno criado com sucesso!');
+						} catch (error) {
+							console.error('Erro ao criar o aluno:', error);
+						}
+					} else {
+						console.log('Aluno já existente!');
+					}
+					break;
+
+				case "03":
+					const cursos = await cursoDAO.findAll();
+					console.log('Cursos cadastrados: ', cursos);
+					break;
+
+				case "04":
+					const alunos = await alunoDAO.findAll();
+					console.log('Alunos cadastrados: ', alunos);
+					break;
+
+				case "05":
+					const alunoBuscaId = parseInt(readlineSync.question('Digite o ID do aluno: '), 10);
+					const alunoEncontrado = await alunoDAO.read(alunoBuscaId);
+					if (alunoEncontrado) {
+						console.log('Aluno encontrado: ', alunoEncontrado);
+
+						const desejaAlterarNome = readlineSync.question('Deseja alterar o nome do aluno? (sim/nao): ');
+						if (desejaAlterarNome.toLowerCase() === 'sim') {
+							const novoNome = readlineSync.question('Qual é o novo nome do aluno? ');
+							alunoEncontrado.nome = novoNome;
+							await alunoDAO.update(alunoEncontrado);
+							console.log('Nome do aluno atualizado!');
+						} else {
+							console.log('Nome do aluno não alterado!');
+						}
+					} else {
+						console.log('Aluno não encontrado');
+					}
+					break;
+
+				case "06":
+					const cursoBuscarId = parseInt(readlineSync.question('Digite o ID do curso: '), 10);
+					const cursoEncontrado = await cursoDAO.read(cursoBuscarId);
+					if (cursoEncontrado) {
+						console.log('Curso encontrado: ', cursoEncontrado);
+					} else {
+						console.log('Curso não encontrado');
+					}
+					break;
+
+				case "00":
+					console.log('Saindo...');
+					break;
+
+				default:
+					console.log('Opção inválida!!!');
+					return;
+			}
 		}
 	} catch (error) {
 		console.error('Erro ao salvar aluno e cursos', error);
 	}
 }
+
 informações();
